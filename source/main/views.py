e8f4bea116e263cef53e9d7c758bf51f34d7c2b8
José Manuel <l21212364@tectijuana.edu.mx>
2025-10-27 19:49:48 -0700

    Add login

---
+++ source/main/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+
+
+class IndexPageView(TemplateView):
+    template_name = "main/index.html"
+
+
+class ChangeLanguageView(TemplateView):
+    template_name = "main/change_language.html"
+
+class views(TemplateView):
+    template_name = "main/index.html"
